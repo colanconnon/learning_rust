a7dda533d4c98e2258f37fbdcfb294809b958533
Colan Connon <cconnon11@gmail.com>
2022-12-10 11:22:24 -0500

    add tests

--- fib/src/main.rs
+++ fib/src/main.rs
@@ -12,7 +12,36 @@ fn fib(n: u64, memo: &mut HashMap<u64, u64>) -> u64 {
         }
     }
 }
-fn main() {
+
+fn calc_fib(n: u64) -> u64 {
     let mut memo: HashMap<u64, u64> = HashMap::from([(0, 0), (1, 1)]);
-    println!("{}", fib(50, &mut memo));
+    return fib(n, &mut memo);
+} 
+fn main() {
+    println!("{}", calc_fib(50));
 }
+
+
+#[cfg(test)]
+mod tests {
+    use crate::calc_fib;
+
+    #[test]
+    fn it_calcs_10() {
+        let result = calc_fib(10);
+        assert_eq!(result, 55);
+    }
+
+    #[test]
+    fn it_calcs_50() {
+        let result = calc_fib(50);
+        assert_eq!(result, 12586269025);
+    }
+
+    #[test]
+    fn it_calcs_1() {
+        let result = calc_fib(1);
+        assert_eq!(result, 1);
+    }
+}
+
